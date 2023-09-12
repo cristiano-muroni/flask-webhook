94016ce02a113db85dd2bdf273d200a77839754e
Cristiano Muroni <53710097+cristiano-muroni@users.noreply.github.com>
2023-09-11 22:21:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
-@app.route('/webhook', methods=['GET'])
+@app.route('/webhooks', methods=['GET'])
 def analyse():     
     hub_challenge = request.json.get('hub.challenge')                   
     return hub_challenge
