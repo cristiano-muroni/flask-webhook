930c495d40b6f85461666eed1bf51fee2d53f7e8
Cristiano Muroni <53710097+cristiano-muroni@users.noreply.github.com>
2023-09-11 21:47:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+@app.route('/webhook', methods=['GET'])
+def analyse():     
+    hub_challenge = request.json.get('hub.challenge')                   
+    return hub_challenge
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
